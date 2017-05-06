915e0c3967bf8543bc887d031f397e84996ebacf
= <zonamp3metal@gmail.com>
2017-05-06 21:41:45 +0700

    new

---
+++ script.js
@@ -0,0 +1,77 @@
+      var app = angular.module("Dion", []);
+      app.controller("Dctrl", function($scope){
+        $scope.metode    = [
+          {
+            "Judul"  : "Rencana",
+            "Gambar" : "https://image.ibb.co/m5hHgQ/plan.jpg",
+            "Deskripsi" : "ketika masuk ke proyek apapun membutuhkan garis besar yang lengkap. Rencana merupakan dasar yang kokoh yang harus pertama kali untuk dikerjakan."
+          },
+          {
+            "Judul"  : "Coding",
+            "Gambar" : "http://img.freepik.com/free-vector/web-page_1300-265.jpg",
+            "Deskripsi" : "Saya biasanya menulis sesuai dengan rencana yang sudah saya rencanakan dan kemudian saya menulis nya dengan berhati-hati untuk menimalisir error"
+          },
+          {
+            "Judul"  : "Belajar",
+            "Gambar" : "http://www.freepik.com/blog/wp-content/uploads/2016/02/24780-NV8FA4-1024x1024.jpg",
+            "Deskripsi" : "Mencatat semua informasi yang diambil dari proyek. Seperti ketika ada error. dan ini sangat bermanfaat sekali jika kita melakukan proyek yang mirip."
+          }
+        ],
+
+        $scope.portofolio = [
+          {
+            "Judul"     : "Kalkulator Sederhana",
+            "Link"      : "http://github.com/dionarya6661/kalkulator",
+            "Gambar"    : "https://image.ibb.co/dcRpVa/capture_20170328_115302.png",
+            "Deskripsi" : "Program Pertama Saya, sebuah program yang simple yaitu Kalkulator Sederhana Dengan bahasa PHP"
+          },
+          {
+            "Judul"     : "EmbahDap",
+            "Link"      : "http://embahdap.hol.es",
+            "Gambar"    : "https://image.ibb.co/bRDgbF/capture_20170328_114651.png",
+            "Deskripsi" : "EmbahDap merupakan aplikasi Web yang bisa meramal, cinta, kematian, kapan putus dan meramal yang lainnya"
+          },
+          {
+            "Judul"     : "DapCMS",
+            "Link"      : "http://github.com/dionarya6661/dapcms",
+            "Gambar"    : "https://image.ibb.co/n8xJOv/capture_20170328_114203.png",
+            "Deskripsi" : "DapCMS merupakan karya saya selanjutnya, ini merupakan sebuah content management system sederhana dibuat dengan mysql dan php"
+          },
+          {
+            "Judul"     : "ColayGram",
+            "Link"      : "http://github.com/dionarya6661/colaygram",
+            "Gambar"    : "https://image.ibb.co/njyxGF/capture_20170328_115701.png",
+            "Deskripsi" : "Terinspirasi film 'The Social Network' saya membuat sebuah social media sederhana yaitu ColayGram, Ini hampir mirip dengan Instagram"
+          },
+          {
+            "Judul"     : "QuranSaya.ID",
+            "Link"      : "https://quransaya.id",
+            "Gambar"    : "https://image.ibb.co/i1Ovov/capture_20170328_145458.png",
+            "Deskripsi" : "Merupakan Aplikasi Web Yang bertujuan untuk membaca Al-Quran Secara Digital. Saya Bangga dengan yang satu ini"
+          },
+          {
+            "Judul"     : "DapCommerce",
+            "Link"      : "#",
+            "Gambar"    : "https://image.ibb.co/jWePgF/capture_20170328_144949.png",
+            "Deskripsi" : "DapCommerce merupakan toko online yang saya buat untuk keperluan kelas saya(XII PM1) yang waktu itu sedang melakukan Pemasaran Online"
+          },
+          {
+            "Judul"     : "PaijoBoth",
+            "Link"      : "https://github.com/dionarya6661/paijoboth",
+            "Gambar"    : "https://image.ibb.co/hyZ38v/capture_20170331_145018.png",
+            "Deskripsi" : "Terinspirasi dari Mark Zuckerberg yang mebuat Jarvis, Saya pun membuat simple personnal assistent yaitu Paijoboth"
+          },
+          {
+            "Judul"     : "DapEmot",
+            "Link"      : "https://github.com/dionarya6661/dap_emot",
+            "Gambar"    : "https://image.ibb.co/hGw4gF/capture_20170328_145653.png",
+            "Deskripsi" : "Sebuah Library Php Untuk mengeluarkan sistem emoticon terdapat 25 emoticon, Karya saya yang ini bisa membantu beberapa orang"
+          },
+          {
+            "Judul"     : "DapChat",
+            "Link"      : "http://github.com/dionarya6661/dapchat",
+            "Gambar"    : "https://image.ibb.co/ipnJOv/capture_20170328_121038.png",
+            "Deskripsi" : "Bosan Dengan bahasa PHP akhirnya saya pun mempelajari Node.JS dan membuat aplikasi chatting ini"
+          }
+        ]
+      });
