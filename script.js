b59ed3ba1adf97d5ff87ad13fe21a0ce7fa76f2d
Dion Arya Pamungkas <dionarya.p@gmail.com>
2017-12-19 10:14:51 +0700

    change to nuxtjs

--- script.js
+++ script.js
@@ -1,73 +1,25 @@
-var appVue = new Vue({
-  el: "#app",
-  data: {
-    metode: [
-          {
-            "Judul"  : "Rencana",
-            "Gambar" : "https://image.ibb.co/m5hHgQ/plan.jpg",
-            "Deskripsi" : "ketika masuk ke proyek apapun membutuhkan garis besar yang lengkap. Rencana merupakan dasar yang kokoh yang harus pertama kali untuk dikerjakan."
-          },
-          {
-            "Judul"  : "Coding",
-            "Gambar" : "https://image.ibb.co/eY2Urv/code.jpg",
-            "Deskripsi" : "Saya biasanya menulis kode sesuai dengan rencana yang sudah saya rencanakan dan kemudian saya mulai menulis kode dengan berhati-hati untuk menimalisir error"
-          },
-          {
-            "Judul"  : "Belajar",
-            "Gambar" : "https://preview.ibb.co/hdHqya/note.jpg",
-            "Deskripsi" : "Mencatat semua informasi yang diambil dari proyek. Seperti ketika ada error. dan ini sangat bermanfaat sekali jika kita melakukan proyek yang mirip."
-          }
-        ],
-
-      karya : [
-          {
-            "Judul"     : "EmbahDap",
-            "Link"      : "http://embahdap.hol.es",
-            "Gambar"    : "https://image.ibb.co/bRDgbF/capture_20170328_114651.png",
-            "Deskripsi" : "EmbahDap merupakan aplikasi Web yang bisa meramal, cinta, kematian, kapan putus dan meramal yang lainnya"
-          },
-          {
-            "Judul"     : "QuranSaya.ID",
-            "Link"      : "https://quransaya.id",
-            "Gambar"    : "https://image.ibb.co/i1Ovov/capture_20170328_145458.png",
-            "Deskripsi" : "Merupakan Aplikasi Web Yang bertujuan untuk membaca Al-Quran Secara Digital. Saya Bangga dengan yang satu ini"
-          },
-          {
-            "Judul"     : "DapCommerce",
-            "Link"      : "#",
-            "Gambar"    : "https://image.ibb.co/jWePgF/capture_20170328_144949.png",
-            "Deskripsi" : "DapCommerce merupakan toko online yang saya buat untuk keperluan kelas saya(XII PM1) yang waktu itu sedang melakukan Pemasaran Online"
-          },
-          {
-            "Judul"     : "url-longer",
-            "Link"      : "https://github.com/dionarya6661/pemanjang-url",
-            "Gambar"    : "https://image.ibb.co/h59QTF/capture_20170527_220832.png",
-            "Deskripsi" : "Web ini saya buat konsepnya hampir mirip dengan url shortener akan tetapi saya sedikit merubah nya pada bagian hasil hash urlnya"
-          },
-          {
-            "Judul"     : "DapEmot",
-            "Link"      : "https://github.com/dionarya6661/dap_emot",
-            "Gambar"    : "https://image.ibb.co/hGw4gF/capture_20170328_145653.png",
-            "Deskripsi" : "Sebuah Library Php Untuk mengeluarkan sistem emoticon terdapat 25 emoticon, Karya saya yang ini bisa membantu beberapa orang"
-          },
-          {
-            "Judul"     : "FACEMASH",
-            "Link"      : "https://dionarya6661.github.io/facemash",
-            "Gambar"    : "https://image.ibb.co/kjOr3F/capture_20170531_201928.png",
-            "Deskripsi" : "Facemash, saya membuat ini terinspirasi dari film The Social Network, disini saya gunakan untuk merating ilmuwan-ilmuwan"
-          },
-          {
-            "Judul"     : "9Kata.com",
-            "Link"      : "http://9kata.com",
-            "Gambar"    : "https://image.ibb.co/hNkwZa/capture_20170705_102330.png",
-            "Deskripsi" : "Merupakan sebuah situs web yang bertujuan untuk sharing kata-kata yang saya buat sendiri, konsep dari web ini sendiri mirip dengan Twitter"
-          },
-          {
-            "Judul"     : "DapTranslate",
-            "Link"      : "https://dap-translate.herokuapp.com",
-            "Gambar"    : "https://image.ibb.co/f7r476/capture_20171217_083915.png",
-            "Deskripsi" : "Merupakan situs web yang mirip dengan google translate, saya membuat ini memakai NodeJS dan Google Translate API"
-          }
-        ]
+document.addEventListener('DOMContentLoaded', function () {
+    
+      // Get all "navbar-burger" elements
+      var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+    
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+          $el.addEventListener('click', function () {
+    
+            // Get the target from the "data-target" attribute
+            var target = $el.dataset.target;
+            var $target = document.getElementById(target);
+    
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+    
+          });
+        });
       }
-  });
+    
+    });
